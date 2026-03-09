919129b25180a8e8d9a9a06ecf160d8b062689c1
Norwegia <65297640+Norwegia@users.noreply.github.com>
2026-03-09 11:00:49 +0100

    fix: add check for being in the right story for internal orders

--- src/elevator.c
+++ src/elevator.c
@@ -162,7 +162,9 @@ check_orders (Elevator *s_elevator, Queue *s_queue)
             printf("reached target order\n");
             return true;
         }
-        else if (current_node->m_order.m_direction == DIRN_STOP)
+        else if (current_node->m_order.m_direction == DIRN_STOP
+                 && current_node->m_order.m_floor
+                        == s_elevator->m_current_floor)
         {
             return true;
         }
